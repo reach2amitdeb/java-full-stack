99633d5778c0e594abbb19e6267c56215f7b707a
Amitdeb <reach2amitdeb@gmail.com>
2020-06-10 22:21:18 -0400

    Created MyDate.java for example information hiding... encapsulation.

---
+++ object-oriented/src/io/workshop/domain/MyDate.java
@@ -0,0 +1,36 @@
+package io.workshop.domain;
+
+public class MyDate {
+	private int day;
+	private int month;
+	private int year;
+	
+	public int getDay() {
+		return day;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setDay(int day) {
+		if (day >0 && day <=31) {  //logic
+			this.day = day;
+		}
+	}
+	
+	public void setMonth(int month) {
+		if(month > 0 && month <=12) {
+			this.month = month;
+		}
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+		
+	}
+}
