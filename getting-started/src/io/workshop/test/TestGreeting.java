026f27eab1df2c5fc91d55aca7de2e5ff96e5a7a
Amitdeb <reach2amitdeb@gmail.com>
2020-06-10 20:38:02 -0400

    created TestGreeting.java file

---
+++ getting-started/src/io/workshop/test/TestGreeting.java
@@ -0,0 +1,12 @@
+package io.workshop.test;
+
+import io.workshop.domain.Greeting;
+
+public class TestGreeting {
+
+	public static void main(String[] args) {
+		Greeting hello = new Greeting();
+		hello.greet();
+	}
+
+}
